d80861c941b4e592c92aa5f5af6001361e98622e
Oded2 <obenamotz@gmail.com>
2025-02-25 14:29:52 +0200

    Added DaisyUI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,13 @@
 "use client";
 
-import Button from "@/components/Button";
 import Container from "@/components/Container";
 
 function Home() {
-  const a = () => {
-    console.log("here");
-  };
   return (
     <Container>
       <div>
         <h1 className="font-bold text-3xl text-center">AstroFetch</h1>
+        <button className="btn btn-primary">Click me</button>
       </div>
     </Container>
   );
