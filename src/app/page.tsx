5a39c65566ba0221fd7e9074ed0205fbca7e70f0
Oded2 <obenamotz@gmail.com>
2025-02-25 14:50:22 +0200

    Modified home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,25 @@
 "use client";
 
-import Container from "@/components/Container";
-
-function Home() {
+export default function Home() {
   return (
-    <Container>
-      <div>
-        <h1 className="font-bold text-3xl text-center">AstroFetch</h1>
-        <button className="btn btn-primary">Click me</button>
+    <div
+      className="hero min-h-screen"
+      style={{
+        backgroundImage:
+          "url(https://upload.wikimedia.org/wikipedia/commons/4/43/ESO-VLT-Laser-phot-33a-07.jpg)",
+      }}
+    >
+      <div className="hero-overlay"></div>
+      <div className="hero-content text-neutral-content text-center">
+        <div className="max-w-md">
+          <h1 className="mb-5 text-5xl font-bold">Explore the Stars</h1>
+          <p className="mb-5">
+            Welcome to AstroFetch. A simple yet effective tool to easily view
+            the Astronomy Picture of the Day (APOD).
+          </p>
+          <button className="btn btn-primary">Get Started</button>
+        </div>
       </div>
-    </Container>
+    </div>
   );
 }
-
-export default Home;
