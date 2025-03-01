4208898925096e1f5a017d29530aedd34d867a16
Oded2 <obenamotz@gmail.com>
2025-03-01 02:04:17 +0200

    Updated UI

--- src/components/ApodCard.tsx
+++ src/components/ApodCard.tsx
@@ -8,12 +8,9 @@ interface Props {
   id?: string;
 }
 
-export function ApodCard({ data, onView, id = "apodCard" }: Props) {
+export function ApodCard({ data, onView, id }: Props) {
   return (
-    <div
-      className="card bg-base-300 w-96 shadow-sm h-full max-h-[80vh] relative overflow-auto"
-      id={id}
-    >
+    <div className="card bg-base-300 w-96 shadow-sm h-full relative" id={id}>
       <figure className="relative aspect-square">
         <Image
           src={data.thumbnail_url ?? data.url ?? "/images/no-image.jpg"}
@@ -26,7 +23,9 @@ export function ApodCard({ data, onView, id = "apodCard" }: Props) {
       <div className="card-body">
         <h2 className="card-title">{data.title}</h2>
         <h3 className="font-medium ">{formatDate(new Date(data.date))}</h3>
-        <p className="line-clamp-3 sm:line-clamp-12">{data.explanation}</p>
+        <p className="line-clamp-3 sm:line-clamp-6 md:line-clamp-8">
+          {data.explanation}
+        </p>
         <div className="card-actions justify-end mt-1">
           <button onClick={onView} className="btn btn-primary">
             View
