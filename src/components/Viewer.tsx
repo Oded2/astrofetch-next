170d3ea843d4b62d7f2165348be9c1550cbc97d7
Oded2 <obenamotz@gmail.com>
2025-03-01 19:31:45 +0200

    Improved image sizing

--- src/components/Viewer.tsx
+++ src/components/Viewer.tsx
@@ -121,7 +121,7 @@ export function Viewer({ apodData, priority, onBack }: Props) {
                 <Photo
                   src={apodData.url}
                   alt={apodData.title}
-                  className="max-h-full object-contain w-full lg:w-auto xl:max-w-4xl 2xl:max-w-5xl lg:min-w-xl xl:min-w-3xl"
+                  className="max-h-full object-contain w-full lg:w-auto xl:max-w-4xl 2xl:max-w-5xl lg:min-w-xl xl:min-w-3xl 2xl:min-w-5xl"
                   priority={priority}
                 ></Photo>
               )}
